a296b770f227c4b428f061b13c736edf7bc25872
Mastrakas <71260092+Mastrakas@users.noreply.github.com>
2020-12-10 17:34:21 +0100

    login + header

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,20 +4,23 @@ namespace App\Controller;
 
 use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController{
 
     /**
-     * @Route("/home", name="home")
+     * @Route("", name="home")
+     * @param ArticleRepository $articleRepository
+     * @return Response
      */
 
     public function articlelist (ArticleRepository $articleRepository){
         $articlesActu = $articleRepository->findBy(
-            ['type_article' => '1']);
+            ['type_article' => '1'], ['date' => 'DESC'], 2);
 
         $articlesTraining = $articleRepository->findBy(
-            ['type_article' => '2']);
+            ['type_article' => '2'], ['date' => 'DESC'], 2);
 
         return $this->render('home.html.twig',
             [
